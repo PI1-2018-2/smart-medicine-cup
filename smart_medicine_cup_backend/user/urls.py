79a24265448aea3ddf933125ad1f38ef5b00042d
Paulo Victor <pv.paulovictor.vp@gmail.com>
2018-11-12 16:55:45 -0200

    Update user login and create user signup

--- smart_medicine_cup_backend/user/urls.py
+++ smart_medicine_cup_backend/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from user.views import Login
+from user.views import Login, SingUp
 
 
 urlpatterns = [
     path('login/', Login.as_view()),
+    path('signup/', SingUp.as_view()),
 ]
