ece659cfba07f8a9fa71f12a6cb0c597db659dfc
Paulo Victor <pv.paulovictor.vp@gmail.com>
2018-11-12 16:55:45 -0200

    Update user login and create user signup

--- smart_medicine_cup_backend/user/views.py
+++ smart_medicine_cup_backend/user/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class Login(TemplateView):
     template_name = "login.html"
+
+class SingUp(TemplateView):
+	template_name = "signup.html"
