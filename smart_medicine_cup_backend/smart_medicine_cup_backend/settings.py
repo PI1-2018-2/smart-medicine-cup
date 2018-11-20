ece63dfdd5547e9d629c86d9102c76acc0a77aef
Gabriel Ziegler <gabrielziegler3@gmail.com>
2018-11-20 19:41:29 -0200

    Update Whitenoise config to suit version >4

--- smart_medicine_cup_backend/smart_medicine_cup_backend/settings.py
+++ smart_medicine_cup_backend/smart_medicine_cup_backend/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'smart_medicine_cup_backend.urls'
