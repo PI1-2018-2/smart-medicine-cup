9d0bef066afbab3cb522c1327911d5634ca1ea58
Gabriel Ziegler <gabrielziegler3@gmail.com>
2018-11-21 11:33:14 -0200

    Fix HTML static pages
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>
    Co-authored-by: Ícaro Pires <icaropsa@gmail.com>

--- smart_medicine_cup_backend/smart_medicine_cup_backend/settings.py
+++ smart_medicine_cup_backend/smart_medicine_cup_backend/settings.py
@@ -16,7 +16,6 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -125,8 +124,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-django_heroku.settings(locals())
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
@@ -134,3 +131,5 @@ STATICFILES_ROOT = (os.path.join(BASE_DIR, "staticfiles"), )
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
