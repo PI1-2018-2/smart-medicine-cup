e3576640239828d8a782483c590af1a65cc40d01
Gabriel Ziegler <gabrielziegler3@gmail.com>
2018-11-21 01:22:32 -0200

    Fix problem with whitenoise for deployment
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- smart_medicine_cup_backend/smart_medicine_cup_backend/settings.py
+++ smart_medicine_cup_backend/smart_medicine_cup_backend/settings.py
@@ -48,13 +48,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'smart_medicine_cup_backend.urls'
@@ -129,4 +129,4 @@ django_heroku.settings(locals())
 STATICFILES_ROOT = (os.path.join(BASE_DIR, "staticfiles"), )
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
