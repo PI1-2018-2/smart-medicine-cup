83a6613fc48e17110becadec6be05fe7757aa030
Rdadamos <rodrigodadamos@gmail.com>
2018-11-21 14:34:13 -0200

    Issue #13 Initial edit registration page

--- smart_medicine_cup_backend/dashboard/urls.py
+++ smart_medicine_cup_backend/dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from dashboard.views import Dashboard
+from dashboard.views import Dashboard, Registration
 
 urlpatterns = [
     path('', Dashboard.as_view()),
+    path('registration/', Registration.as_view()),
 ]
--- smart_medicine_cup_backend/dashboard/views.py
+++ smart_medicine_cup_backend/dashboard/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class Dashboard(TemplateView):
     template_name = "dashboard.html"
+
+class Registration(TemplateView):
+    template_name = "registration.html"
