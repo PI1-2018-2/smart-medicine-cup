71e491b5e8e8338b449b256c99f8e3df30358b45
Gabriel Ziegler <gabrielziegler3@gmail.com>
2018-11-22 19:52:46 -0200

    Add Alarm model and refactor Partition
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- smart_medicine_cup_backend/smc/models.py
+++ smart_medicine_cup_backend/smc/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=200)
     phone = models.CharField(max_length=30)
-    email = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
 
     def __str__(self):
         return self.name
@@ -20,12 +20,22 @@ class Cup(models.Model):
 
 
 class Partition(models.Model):
-    #  explicitly define id so we can return it in __str__
-    id = models.AutoField(primary_key=True)
     cup = models.ForeignKey(Cup, on_delete=models.CASCADE)
-    medicine_time = models.TimeField()
-    medicine_date = models.DateTimeField()
     was_taken = models.BooleanField()
+    message_moment = models.TimeField()
 
     def __str__(self):
-        return "{} {}".format(medicine_date, medicine_time)
+        return self.id
+
+
+class Alarm(models.Model):
+    partition = models.ForeignKey(Partition, on_delete=models.CASCADE)
+    start_time = models.TimeField()
+    period = models.TimeField()
+    # Duration
+    day_length = models.IntegerField()
+    # Registered or cancelled
+    event = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Start time {self.start_time}, Period {self.period} for {self.day_length} day(s)"
