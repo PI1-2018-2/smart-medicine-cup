d0af33c2dc1c23b4c613ce278ba0a7e80c693dfb
Hiroshi <lucas.hh@hotmail.com>
2018-11-25 20:47:55 -0200

    Add listing to dashboard

--- smart_medicine_cup_backend/notification_telegram/resources.py
+++ smart_medicine_cup_backend/notification_telegram/resources.py
@@ -1,8 +1,8 @@
 # api/resources.py
 from tastypie.resources import ModelResource
-from .models import Note
+from cups.models import Record
 
 class NotificationResource(ModelResource):
     class Meta:
-        queryset = Note.objects.all()
+        queryset = Record.objects.all()
         resource_name = 'notification'
