9e24990e56e65bdc8847f92cc59f0b2cd8165501
Rdadamos <rodrigodadamos@gmail.com>
2018-11-25 18:53:34 -0200

    Navigation links adjustments

--- smart_medicine_cup_backend/dashboard/urls.py
+++ smart_medicine_cup_backend/dashboard/urls.py
@@ -3,7 +3,6 @@ from dashboard import views
 
 urlpatterns = [
     path('', views.Dashboard.as_view()),
-    path('registration/', views.Registration.as_view()),
     path('edit_registration/', views.EditRegistration.as_view()),
     path('login/', views.Login.as_view()),
     path('smc/', views.Smc.as_view()),
