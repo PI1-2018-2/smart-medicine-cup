1afb8df337e53068c076b5819fa636985ec5b7a8
Gabriel Ziegler <gabrielziegler3@gmail.com>
2018-11-25 19:19:28 -0200

    Fix user login and logout
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- smart_medicine_cup_backend/smc/views/user.py
+++ smart_medicine_cup_backend/smc/views/user.py
@@ -1,8 +1,7 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
 
 from ..forms.user import SignUpForm
 
@@ -16,7 +15,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('/')
+            return redirect('/dashboard/')
     else:
         form = SignUpForm()
     return render(request, 'registration.html', {'form': form})
@@ -24,24 +23,22 @@ def signup(request):
 
 def user_login(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(
-                request,
-                username=username,
-                password=password
-            )
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
 
-            if user:
-                login(request, user)
-                return HttpResponseRedirect(reverse('dashboard/'))
-            else:
-                print('Error login')
+        if user:
+            login(request, user)
+            return redirect('/dashboard/')
+        else:
+            return redirect('/dashboard/login')
 
 
 @login_required
 def user_logout(request):
     logout(request)
-    return HttpResponseRedirect(reverse('/'))
+    return redirect('/')
