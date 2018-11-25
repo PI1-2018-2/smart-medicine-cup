a4edc2b4f51c3e2eae911856a831e97a75f5fa99
Rdadamos <rodrigodadamos@gmail.com>
2018-11-25 17:45:25 -0200

    Inputs form signup adjustment

--- smart_medicine_cup_backend/smc/forms/user.py
+++ smart_medicine_cup_backend/smc/forms/user.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    username = forms.CharField(max_length=30, help_text='Obrigatório.', widget=forms.TextInput(attrs={'class':'form-control'}), label='Nome de usuário')
+    first_name = forms.CharField(max_length=30, help_text='Obrigatório.', widget=forms.TextInput(attrs={'class':'form-control'}), label='Nome')
+    last_name = forms.CharField(max_length=30,  help_text='Obrigatório.', widget=forms.TextInput(attrs={'class':'form-control'}), label='Sobrenome')
+    email = forms.EmailField(max_length=254, help_text='Obrigatório. Informar um e-mail válido.', widget=forms.TextInput(attrs={'class':'form-control'}), label='E-mail')
+    password1 = forms.CharField(max_length=30, help_text='Obrigatório.', widget=forms.TextInput(attrs={'class':'form-control', 'type':'password'}), label='Senha')
+    password2 = forms.CharField(max_length=30, help_text='Obrigatório.', widget=forms.TextInput(attrs={'class':'form-control', 'type':'password'}), label='Confirmar senha')
 
     class Meta:
         model = User
