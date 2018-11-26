e583e9cfd544cac308571cbf6fbb8d2203e7466b
Rdadamos <rodrigodadamos@gmail.com>
2018-11-26 00:10:24 -0200

    Registration page adjustment

--- smart_medicine_cup_backend/smc/forms/user.py
+++ smart_medicine_cup_backend/smc/forms/user.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=30, help_text='Obrigatório.', widget=forms.TextInput(attrs={'class':'form-control'}), label='Nome de usuário')
-    first_name = forms.CharField(max_length=30, help_text='Obrigatório.', widget=forms.TextInput(attrs={'class':'form-control'}), label='Nome')
-    last_name = forms.CharField(max_length=30,  help_text='Obrigatório.', widget=forms.TextInput(attrs={'class':'form-control'}), label='Sobrenome')
-    email = forms.EmailField(max_length=254, help_text='Obrigatório. Informar um e-mail válido.', widget=forms.TextInput(attrs={'class':'form-control'}), label='E-mail')
-    password1 = forms.CharField(max_length=30, help_text='Obrigatório.', widget=forms.TextInput(attrs={'class':'form-control', 'type':'password'}), label='Senha')
-    password2 = forms.CharField(max_length=30, help_text='Obrigatório.', widget=forms.TextInput(attrs={'class':'form-control', 'type':'password'}), label='Confirmar senha')
+    username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control'}), label='Nome de usuário', error_messages={'required': 'Por favor, informe um nome que será usado no login.', 'max_length': 'Por favor, escolher um nome menor.'})
+    first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control'}), label='Nome')
+    last_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control'}), label='Sobrenome')
+    email = forms.EmailField(max_length=254, widget=forms.TextInput(attrs={'class':'form-control'}), label='E-mail')
+    password1 = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control', 'type':'password'}), label='Senha')
+    password2 = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control', 'type':'password'}), label='Confirmar senha')
 
     class Meta:
         model = User
