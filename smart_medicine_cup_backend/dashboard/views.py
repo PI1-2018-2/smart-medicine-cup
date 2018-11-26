ccc648ae10658597f56e912ff72a2253946ab85b
Hiroshi <lucas.hh@hotmail.com>
2018-11-26 02:07:11 -0200

    Create view for phone

--- smart_medicine_cup_backend/dashboard/views.py
+++ smart_medicine_cup_backend/dashboard/views.py
@@ -1,24 +1,33 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView, CreateView, FormView, View
 from django.utils.decorators import method_decorator
 
 # Local Django imports
-from cups.models import Cup,Record
+from cups.models import Cup, Record
+from cups.forms import CupForm
 
-class Dashboard(ListView):
+class Dashboard(View):
+    """
+    Renders the home page (dashboard) of the health professional.
+    """
     template_name = 'dashboard.html'
-    context_object_name = 'list_record'
-    model = Record 
-    ordering = ['-date_created']
+
 
     def dispatch(self, *args, **kwargs):
         return super(Dashboard, self).dispatch(*args, **kwargs)
 
-    def get_queryset(self):
-        #return self.model.objects.filter(user_creator=self.request.user)
-        print(self.model.objects.all())
-        return self.model.objects.all()
+    def get(self, request):
+
+        # Set initial date first hour
+        list_record = Record.objects.all()
+
+        # Get six latest prescriptions
+        list_cups = Cup.objects.filter(user_id=self.request.user)
+
+        return render(request, self.template_name, {'list_record': list_record,
+                                                    'list_cups': list_cups})
+
+
 
 class EditRegistration(TemplateView):
     template_name = "edit_registration.html"
@@ -26,8 +35,22 @@ class EditRegistration(TemplateView):
 class Login(TemplateView):
     template_name = "login.html"
 
-class AddSmc(TemplateView):
+
+class AddSmc(FormView):
     template_name = "addsmc.html"
+    form_class = CupForm
+    success_url = '/dashboard/smc'
+    def dispatch(self, *args, **kwargs):
+
+        return super(AddSmc, self).dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        self.cup = form.save(commit=False)
+        self.cup.user_id = self.request.user.id
+        self.cup.save()
+
+        return super(AddSmc, self).form_valid(form)
+
 
 class Smc(ListView):
     template_name = "smc.html"
@@ -39,7 +62,6 @@ class Smc(ListView):
         return super(Smc, self).dispatch(*args, **kwargs)
 
     def get_queryset(self):
-        print(self.model.objects.all())
         return self.model.objects.filter(user=self.request.user)
 
 class Historic(ListView):
