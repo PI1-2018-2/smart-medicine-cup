acd01f2e7997471d6fc9440f25b0d5debbe1df2f
Paulo Victor <pv.paulovictor.vp@gmail.com>
2018-11-28 17:25:52 -0200

    Created view cup

--- smart_medicine_cup_backend/smc/forms/cup.py
+++ smart_medicine_cup_backend/smc/forms/cup.py
@@ -4,7 +4,10 @@ from models import Cup
 
 class RegistrationCup():
     code = forms.CharField(max_length=10, widget=forms.TextInput(attrs={'class':'form-control'}), label='Código do Copo', error_messages={'required': 'Por favor, informe o código do seu SMC.'})
+    name = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class':'form-control'}), label='Nome do Contato', error_messages={'required': 'Por favor, informe o nome do seu contato para o SMC.'})
+    phone = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control'}), label='Telefone do Contato', error_messages={'required': 'Por favor, informe o número do seu contato para o SMC.'})
+    email = forms.EmailField(max_length=200, widget=forms.TextInput(attrs={'class':'form-control'}), label='Email do Contato', error_messages={'required': 'Por favor, informe o emails do seu contato para o SMC.'})
 
     class Meta:
         model = Cup
-        fields = ('code')
+        fields = ('code', 'name', 'phone', 'email')
---
+++ smart_medicine_cup_backend/smc/views/cup.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.decorators import login_required
+
+from ..models import Cup, Contact
+from ..forms.cup import RegistrationCup
+
+
+@login_required
+def register_cup(request):
+    if request.method == 'POST':
+        form = RegistrationCup(request.POST)
+        if form.is_valid():
+            form.save()
+            code = request.POST.get('code')
+            name = request.POST.get('name')
+            phone = request.POST.get('phone')
+            email = request.POST.get('email')
+
+            contact = Contact(name=name, phone=phone, email=email)
+            contact.save()
+            cup = Cup(cup.user.add(user), contact=contact.primary_key)
+            cup.save()
+            return redirect('/dashboard/')
+    else:
+        form = RegistrationCup()
+    return redirect('/dashboard/')
