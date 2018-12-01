9913f3f71cdf21e7652c5f70f0279f8b467e7c29
Gabriel Ziegler <gabrielziegler3@gmail.com>
2018-12-01 17:49:04 -0200

    Improve HTTP responses and remove update alarm from 'event: taken'

--- smart_medicine_cup_backend/smc/views/alarm.py
+++ smart_medicine_cup_backend/smc/views/alarm.py
@@ -1,5 +1,4 @@
 import json
-from datetime import datetime
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ObjectDoesNotExist
@@ -9,35 +8,44 @@ from ..models import User, Cup, Contact, Alarm, Record
 
 @csrf_exempt
 def register_info(request):
+    if request.method != 'POST':
+        return HttpResponse(status=405, content={'error': 'wrong method, use POST'})
+
+    if request.content_type != 'application/json':
+        print(request.content_type)
+        return HttpResponse(status=415, content={'error': 'wrong content-type, use application/json'})
+
     try:
         data = json.loads(request.body)
-    except ValueError as e:
-        return HttpResponse(status=413)
+    except ValueError:
+        return HttpResponse(status=400, content={'error': 'wrong JSON format'})
 
     try:
-        cup = Cup.objects.get(pk=data['id_cup'])
-    except Exception as e:
-        return HttpResponse(status=204)
+        Cup.objects.get(pk=data['id_cup'])
+    except ObjectDoesNotExist:
+        return HttpResponse(status=404, content={'error': 'cannot find cup id'})
 
     # Decides which action to take based on event
-    response_code = event_handler(data)
-    return HttpResponse(status=response_code, content=request.body)
+    return event_handler(data)
 
 
 def event_handler(data):
     if data['event'] == 'registered':
         return register_alarm(data)
-    elif data['event'] == 'taken':
-        return update_alarm(data)
-    elif data['event'] == 'not_taken':
+    if data['event'] == 'taken':
+        return update_record(data)
+    if data['event'] == 'not_taken':
         return alert_contact(data)
-    elif data['event'] == 'cancelled':
+    if data['event'] == 'cancelled':
         return cancel_alarm(data)
-    else:
-        return 400
+
+    return HttpResponse(status=400, content={'error': 'cannot handle event'})
 
 
 def register_alarm(data):
+    if data['partition'] > 4 or data['partition'] < 1:
+        return HttpResponse(status=400, content={'error': 'wrong partition range'})
+
     alarm = Alarm(
         cup=Cup.objects.get(id=data['id_cup']),
         partition=data['partition'],
@@ -55,55 +63,65 @@ def register_alarm(data):
     )
     alarm_record.save()
 
-    return 201
+    return HttpResponse(status=201)
 
 
 def cancel_alarm(data):
     try:
-        alarm = Alarm.objetcs.get(cup=data['id_cup'])
-    except:
-        return 204
-    alarm.is_active = False
-    alarm.save()
-    update_record(alarm.id, data)
+        alarm = Alarm.objects.filter(cup=data['id_cup'], partition=data['partition']).last()
+    except ObjectDoesNotExist:
+        return HttpResponse(status=404, content={'error': 'Cannot find alarm'})
 
-    return 200
+    if alarm:
+        alarm.is_active = False
+        alarm.save()
+        update_record(data)
+        return HttpResponse(status=201)
+
+    return HttpResponse(status=404, content={'error': 'Cannot find alarm'})
 
 
 def update_alarm(data):
-    cup_alarm = Alarm.objects.filter(cup=data['id_cup'], partition=data['partition']).last()
+    alarm = Alarm.objects.filter(cup=data['id_cup'], partition=data['partition']).last()
 
-    if not cup_alarm:
-        return 204
+    if not alarm:
+        return HttpResponse(status=404, content={'error': 'Cannot find alarm'})
 
     start_time = f"{data['alarm_info']['start']['hour']}:{data['alarm_info']['start']['minute']}:00"
-    cup_alarm.start_time = start_time
+    alarm.start_time = start_time
 
     period = f"{data['alarm_info']['period']['hour']}:{data['alarm_info']['period']['minute']}:00"
-    cup_alarm.period = period
+    alarm.period = period
 
-    cup_alarm.duration = data['alarm_info']['duration']
-    cup_alarm.is_active = True
+    alarm.duration = data['alarm_info']['duration']
+    alarm.is_active = True
 
-    cup_alarm.save()
-    update_record(cup_alarm.id, data)
+    alarm.save()
+    update_record(data)
 
-    return 200
+    return HttpResponse(status=201)
 
 
-def update_record(alarm_id, data):
+def update_record(data):
     try:
-        alarm_record = Record.objects.get(alarm=alarm_id)
-    except:
-        return 204
+        alarm = Alarm.objects.filter(cup=data['id_cup'], partition=data['partition']).last()
+    except ObjectDoesNotExist:
+        return HttpResponse(status=404, content={'error': 'Cannot find alarm'})
+
+    try:
+        alarm_record = Record.objects.get(alarm=alarm.id)
+    except (ObjectDoesNotExist, AttributeError):
+        return HttpResponse(status=404, content={'error': 'Cannot find alarm\'s record'})
+
     alarm_record.event = data['event']
     alarm_record.moment = data['moment']
     alarm_record.save()
 
-    return 200
+    return HttpResponse(status=201)
 
 
 def alert_contact(data):
-    cup = Cup.objects.get(id=data['id_cup'])
-    contact = cup.contact
+    pass
+    #  cup = Cup.objects.get(id=data['id_cup'])
+    #  contact = cup.contact
     # TODO Send message to contact
