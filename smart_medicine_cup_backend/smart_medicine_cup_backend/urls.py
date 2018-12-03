d906ddc31ebb9eae97a4c79cebb40eb2aee5c5b6
Hiroshi <lucas.hh@hotmail.com>
2018-12-03 11:26:01 -0200

    Remove resources

--- smart_medicine_cup_backend/smart_medicine_cup_backend/urls.py
+++ smart_medicine_cup_backend/smart_medicine_cup_backend/urls.py
@@ -2,17 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url, include
 
-from smc.resources import RecordResource, CupResource
-
-record_resource = RecordResource()
-
-cup_resource = CupResource()
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing_page.urls')),
     path('api/', include('smc.urls')),
     path('dashboard/', include('dashboard.urls')),
-    url(r'^notification/', include(record_resource.urls)),
-    url(r'^notification/', include(cup_resource.urls)),
 ]
