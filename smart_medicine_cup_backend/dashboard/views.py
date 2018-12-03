7ca8ac6f47939504eabcdabeaaa5aa4f633a5030
Hiroshi <lucas.hh@hotmail.com>
2018-12-02 23:40:54 -0200

    Add all views for list dashboard

--- smart_medicine_cup_backend/dashboard/views.py
+++ smart_medicine_cup_backend/dashboard/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, View
 from smc.models import Contact, Cup, Record, Alarm
 
 # Create your views here.
 class Dashboard(TemplateView):
     template_name = "dashboard.html"
+
+class Dashboard(View):
+    template_name = "dashboard.html"
+
+    def get(self, request):
+        user = request.user
+
+        list_smc = Cup.objects.filter(user=self.request.user)
+        list_historic= Record.objects.filter(alarm__cup__user=self.request.user)
+        list_contacts= Contact.objects.filter(user=self.request.user)
+
+        return render(request, self.template_name, {'list_smc':      list_smc,
+                                                    'list_historic': list_historic,
+                                                    'list_contacts': list_contacts})
 
 class EditRegistration(TemplateView):
     template_name = "edit_registration.html"
@@ -26,8 +40,7 @@ class Historic(ListView):
     model = Record
 
     def get_queryset(self):
-        cups = Cup.objects.filter(user=self.request.user)
-        return self.model.objects.filter(cup_id=cups)
+        return self.model.objects.filter(alarm__cup__user=self.request.user)
 
 class Contacts(ListView):
     template_name = "contacts.html"
