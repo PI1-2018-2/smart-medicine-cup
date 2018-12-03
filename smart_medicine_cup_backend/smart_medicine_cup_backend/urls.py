401ed18f5460f88facfd90b3cd47401002c05c8e
Hiroshi <lucas.hh@hotmail.com>
2018-12-03 00:52:25 -0200

    Add urls for add smc and contact

--- smart_medicine_cup_backend/smart_medicine_cup_backend/urls.py
+++ smart_medicine_cup_backend/smart_medicine_cup_backend/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url, include
 
-from smc.resources import RecordResource
+from smc.resources import RecordResource, ContactResource
+
 record_resource = RecordResource()
+contact_resource = ContactResource()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +13,5 @@ urlpatterns = [
     path('api/', include('smc.urls')),
     path('dashboard/', include('dashboard.urls')),
     url(r'^notification/', include(record_resource.urls)),
+    url(r'^notification/', include(contact_resource.urls)),
 ]
