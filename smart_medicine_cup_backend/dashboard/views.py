209364e2c29ec3ea1c15615a9ee7f6757b4a8f96
Hiroshi <lucas.hh@hotmail.com>
2018-12-03 01:55:34 -0200

    Add list for smc

--- smart_medicine_cup_backend/dashboard/views.py
+++ smart_medicine_cup_backend/dashboard/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, View
-from smc.models import Contact, Cup, Record, Alarm
+from smc.models import Cup, Record, Alarm
 
 # Create your views here.
 class Dashboard(TemplateView):
     template_name = "dashboard.html"
-
-class Dashboard(View):
-    template_name = "dashboard.html"
-
-    def get(self, request):
-        user = request.user
-
-        list_smc = Cup.objects.filter(user=self.request.user)
-        list_historic= Record.objects.filter(alarm__cup__user=self.request.user)
-        list_contacts= Contact.objects.filter(user=self.request.user)
-
-        return render(request, self.template_name, {'list_smc':      list_smc,
-                                                    'list_historic': list_historic,
-                                                    'list_contacts': list_contacts})
-
+                                                    
 class EditRegistration(TemplateView):
     template_name = "edit_registration.html"
 
@@ -39,8 +25,13 @@ class Historic(ListView):
     context_object_name = 'list_historic'
     model = Record
 
-class Alarms(TemplateView):
+    def get_queryset(self):
+        return self.model.objects.filter(alarm__cup__user=self.request.user)
+
+class Alarms(ListView):
     template_name = "alarms.html"
+    model = Alarm
+    context_object_name = 'list_alarm'
 
     def get_queryset(self):
-        return self.model.objects.filter(user=self.request.user)
+        return self.model.objects.filter(cup__user=self.request.user)
