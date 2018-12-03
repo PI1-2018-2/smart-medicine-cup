4495014589c7b5f4bb429bb6dc62f7ce31d74c19
Hiroshi <lucas.hh@hotmail.com>
2018-12-03 01:55:45 -0200

    Add models in admin

--- smart_medicine_cup_backend/smc/admin.py
+++ smart_medicine_cup_backend/smc/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Contact, Cup, Alarm, Record
+from .models import Cup, Alarm, Record
 
-admin.site.register(Contact)
 admin.site.register(Cup)
 admin.site.register(Alarm)
 admin.site.register(Record)
